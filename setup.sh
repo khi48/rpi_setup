22e017221703039884c534f44ebdd08b9a9ffb76
khi48 <kieranhitchcock28@gmail.com>
2024-12-20 21:37:40 +1300

    annoying work around for poetry

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REAL_USER="kieranhitchcock"
+
 sudo apt install git
 
 echo "-----------------------"
@@ -8,10 +10,20 @@ echo "-----------------------"
 sudo apt install -y docker
 sudo apt install -y docker-compose
 sudo groupadd docker
-sudo usermod -aG docker ${USER}
-su -s ${USER}
+sudo usermod -aG docker ${REAL_USER}
+su -s ${REAL_USER}
 docker login
 
 curl -sSL https://install.python-poetry.org | python3 -
+
+
+# Updating bashrc
+cat <<EOT >> /home/kieranhitchcock/.bashrc
+# poetry export
+export PATH="/home/kieranhitchcock/.local/bin:$PATH"
+alias p="poetry run python"
+#poetry config virtualenvs.in-project true
+poetry config keyring.enabled false
+EOT
 
 sudo apt install cowsay
